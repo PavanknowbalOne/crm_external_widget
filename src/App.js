7f921460e651c220dce9c7676d26f839e9ca07f6
Pavan Kumar Yaragogu <pavan@knowbal.com.au>
2025-12-05 13:46:38 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Knowbal One crm
+          Knowbal
         </p>
         <a
           className="App-link"
